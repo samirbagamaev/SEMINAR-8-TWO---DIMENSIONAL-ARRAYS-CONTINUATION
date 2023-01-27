9333c09321718c57a8600ff0764896d6f168f8af
Samir Bagamaev <05ru4315@gmail.com>
2023-01-28 01:05:40 +0300

    WORK_57

---
+++ WORK_57/Program.cs
@@ -0,0 +1,60 @@
+﻿// Составить частотный словарь элементов двумерного массива.
+// Частотный словарь содержит информацию о том, сколько раз
+// встречается элемент входных данных.
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Count(int[,] matrix)
+{
+
+    int[] counters = new int[10];
+
+
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int index = matrix[i, j];
+            counters[index]++;
+        } 
+    }
+    for(int i = 0; i< counters.Length; i++) 
+    {
+        Console.WriteLine($"Элемент {i} встречается {counters[i]} раз");
+    }
+}
+
+const int ROWS = 3;
+const int COLUMNS = 5;
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 9;
+
+int[,] array = GetRandomMatrix(ROWS, COLUMNS, LEFTRANGE, RIGHTRANGE);
+PrintMatrix(array);
+Console.WriteLine();
+Count(array);
